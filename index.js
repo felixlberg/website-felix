49fd6d92c6a58981e08f999f46c56ccbd18644ef
Felix <felix.lberg@gmail.com>
2019-03-12 12:21:31 +0100

    added right netlify auth path

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const SIMPLA_CONFIG = {
   repo: 'felixlberg/felixlberg.github.io',
-  auth: new SimplaNetlify({ site: 'simpla-theme.netlify.com' }),
+  auth: new SimplaNetlify({ site: 'underground-consulting.netlify.com' }),
   source: window.location.origin,
   branch: 'master'
 };
