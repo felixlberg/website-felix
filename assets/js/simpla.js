4ae16a00ddb4177103a69a94023316fdbb4bb181
harald <felix.langenberg@protonmail.com>
2020-04-19 16:37:25 +0200

    changed simpla.js

--- assets/js/simpla.js
+++ assets/js/simpla.js
@@ -3,11 +3,11 @@
 import Simpla from 'simpla';
 import SimplaNetlify from 'simpla/adapters/netlify';
 // In-Context Edit Configuration by Simplajs https://github.com/simplajs/simpla
-const SIMPLA_CONFIG = {
+Simpla.init({
   repo: 'felixlberg/website-felix',
   auth: new SimplaNetlify({ site: 'underground-consulting.netlify.com' }),
   source: 'https://underground-consulting.netlify.com',
   branch: 'master'
-};
+});
 // Simplajs Init
-Simpla.init(SIMPLA_CONFIG);
+window.Simpla = Simpla;
