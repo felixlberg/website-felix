3173be7a5784b292dfb1484ea1f2a58e89773a5c
harald <felix.langenberg@protonmail.com>
2020-04-19 16:27:41 +0200

    added imports

--- assets/js/simpla.js
+++ assets/js/simpla.js
@@ -1,4 +1,7 @@
 'use strict';
+// Import Simpla and OAuth adapter
+import Simpla from 'simpla';
+import SimplaNetlify from 'simpla/adapters/netlify';
 // In-Context Edit Configuration by Simplajs https://github.com/simplajs/simpla
 const SIMPLA_CONFIG = {
   repo: 'felixlberg/website-felix',
