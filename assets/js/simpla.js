dc33e1e8a30b5d44ac6dfe728506b582b3d88cbb
harald <felix.langenberg@protonmail.com>
2020-04-19 16:43:32 +0200

    test

--- assets/js/simpla.js
+++ assets/js/simpla.js
@@ -1,13 +1,10 @@
 'use strict';
-// Import Simpla and OAuth adapter
-import Simpla from 'simpla';
-import SimplaNetlify from 'simpla/adapters/netlify';
 // In-Context Edit Configuration by Simplajs https://github.com/simplajs/simpla
-Simpla.init({
+const SIMPLA_CONFIG = {
   repo: 'felixlberg/website-felix',
-  auth: new SimplaNetlify({ site: 'underground-consulting.netlify.com' }),
-  source: 'https://underground-consulting.netlify.com',
+  auth: new SimplaNetlify({ site: 'underground-consulting' }),
+  source: 'https://underground-consulting',
   branch: 'master'
-});
+};
 // Simplajs Init
-window.Simpla = Simpla;
+Simpla.init(SIMPLA_CONFIG);
