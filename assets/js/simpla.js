68ce996821010029834f68d14a869485243ed329
harald <felix.langenberg@protonmail.com>
2020-04-19 16:46:53 +0200

    test

--- assets/js/simpla.js
+++ assets/js/simpla.js
@@ -2,8 +2,8 @@
 // In-Context Edit Configuration by Simplajs https://github.com/simplajs/simpla
 const SIMPLA_CONFIG = {
   repo: 'felixlberg/website-felix',
-  auth: new SimplaNetlify({ site: 'underground-consulting' }),
-  source: 'https://underground-consulting',
+  auth: new SimplaNetlify({ site: 'underground-consulting.netlify.com' }),
+  source: 'https://underground-consulting.netlify.com',
   branch: 'master'
 };
 // Simplajs Init
