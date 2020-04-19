a8a0f498d1a3a86521e670c7adde279aa68c5ddd
harald <felix.langenberg@protonmail.com>
2020-04-19 16:58:27 +0200

    changed netlify domain

--- assets/js/simpla.js
+++ assets/js/simpla.js
@@ -2,8 +2,8 @@
 // In-Context Edit Configuration by Simplajs https://github.com/simplajs/simpla
 const SIMPLA_CONFIG = {
   repo: 'felixlberg/website-felix',
-  auth: new SimplaNetlify({ site: 'underground-consulting.netlify.com' }),
-  source: 'https://underground-consulting.netlify.com',
+  auth: new SimplaNetlify({ site: 'underground-consulting.netlify.app' }),
+  source: 'https://underground-consulting.netlify.app',
   branch: 'master'
 };
 // Simplajs Init
